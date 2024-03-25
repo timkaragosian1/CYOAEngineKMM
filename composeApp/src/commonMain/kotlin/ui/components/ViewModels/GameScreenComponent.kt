fdfc09b9ff04f1a2cfe6da9c729bff31a56a5487
timkaragosian1 <timkaragosian@gmail.com>
2024-03-25 17:01:18 -0400

    wired data back to Root Component. Tons of crappy boilerplate, but seemed necessary...

--- composeApp/src/commonMain/kotlin/ui/components/ViewModels/GameScreenComponent.kt
+++ composeApp/src/commonMain/kotlin/ui/components/ViewModels/GameScreenComponent.kt
@@ -20,16 +20,47 @@ import ui.components.GameScreen.GameScreenEvent
 @ExperimentalResourceApi
 class GameScreenComponent(
     componentContext: ComponentContext,
+    private val initEventType: String,
+    private val gameEventMessage:String,
+    private val gameEventImage: DrawableResource,
+    private val gameEventType:String,
+    private val gameEventLocation:String,
+    private val gameNextEvent:Int,
+    private val gameButton1Text:String,
+    private val gameButton2Text:String,
+    private val gameButton3Text:String,
+    private val gameButton4Text:String,
+    private val gameButton5Text:String,
+    private val gameButton6Text:String,
+    private val gameButton1Enabled:Boolean,
+    private val gameButton2Enabled:Boolean,
+    private val gameButton3Enabled:Boolean,
+    private val gameButton4Enabled:Boolean,
+    private val gameButton5Enabled:Boolean,
+    private val gameButton6Enabled: Boolean,
+    private val gameButton1NextEvent:Int,
+    private val gameButton2NextEvent:Int,
+    private val gameButton3NextEvent:Int,
+    private val gameButton4NextEvent:Int,
+    private val gameButton5NextEvent:Int,
+    private val gameButton6NextEvent:Int,
+    private val gameCompanyFinances:Long,
+    private val gameGameStatus:String,
+    private val gameShipCrewStatus:Int,
+    private val gameShipHullStatus:Int,
+    private val gameShipEnginesStatus:Int,
+    private val gameShipSensorStatus:Int,
+    private val gameDestinationStatus: String,
+    private val gameGameTime:Double,
+    private val gameCeoFirstname:String,
+    private val gameCeoLastName:String,
+    private val gameCompanyName:String,
     private val onClickButton1: () -> Unit,
     private val onClickButton2: () -> Unit,
     private val onClickButton3: () -> Unit,
     private val onClickButton4: () -> Unit,
     private val onClickButton5: () -> Unit,
     private val onClickButton6: () -> Unit,
-    private val gameCEOFirstname:String,
-    private val gameCEOLastname:String,
-    private val gameCompanyName: String,
-    private val initEventType: String,
     private val onNavigateBackToTitleScreen: () -> Unit
 ): ComponentContext by componentContext {
     private var _eventMessage = MutableValue("Welcome new user! Please press the ID Scan Sign In button to continue.")
@@ -38,72 +69,71 @@ class GameScreenComponent(
     var eventImage: Value<DrawableResource> = _eventImage
     private var _eventType = MutableValue(initEventType)
     var eventType:Value<String> = _eventType
-    private var _eventLocation = MutableValue("")
+    private var _eventLocation = MutableValue(gameEventLocation)
     var eventLocation:Value<String> = _eventLocation
-    private var _nextEvent = MutableValue(0)
+    private var _nextEvent = MutableValue(gameNextEvent)
     var nextEvent:Value<Int> = _nextEvent
 
-    private var _button1Text = MutableValue("")
+    private var _button1Text = MutableValue(gameButton1Text)
     var button1Text:Value<String> = _button1Text
-    private var _button2Text = MutableValue("")
+    private var _button2Text = MutableValue(gameButton2Text)
     var button2Text:Value<String> = _button2Text
-    private var _button3Text = MutableValue("")
+    private var _button3Text = MutableValue(gameButton3Text)
     var button3Text:Value<String> = _button3Text
-    private var _button4Text = MutableValue("")
+    private var _button4Text = MutableValue(gameButton4Text)
     var button4Text:Value<String> = _button4Text
-    private var _button5Text = MutableValue("")
+    private var _button5Text = MutableValue(gameButton5Text)
     var button5Text:Value<String> = _button5Text
-    private var _button6Text = MutableValue("ID Scan\n" +
-            "Sign In")
+    private var _button6Text = MutableValue(gameButton6Text)
     var button6Text:Value<String> = _button6Text
 
-    private var _button1Enabled = MutableValue(false)
+    private var _button1Enabled = MutableValue(gameButton1Enabled)
     var button1Enabled:Value<Boolean> = _button1Enabled
-    private var _button2Enabled = MutableValue(false)
+    private var _button2Enabled = MutableValue(gameButton2Enabled)
     var button2Enabled:Value<Boolean> = _button2Enabled
-    private var _button3Enabled = MutableValue(false)
+    private var _button3Enabled = MutableValue(gameButton3Enabled)
     var button3Enabled:Value<Boolean> = _button3Enabled
-    private var _button4Enabled = MutableValue(false)
+    private var _button4Enabled = MutableValue(gameButton4Enabled)
     var button4Enabled:Value<Boolean> = _button4Enabled
-    private var _button5Enabled = MutableValue(false)
+    private var _button5Enabled = MutableValue(gameButton5Enabled)
     var button5Enabled:Value<Boolean> = _button5Enabled
-    private var _button6Enabled = MutableValue(true)
+    private var _button6Enabled = MutableValue(gameButton6Enabled)
     var button6Enabled:Value<Boolean> = _button6Enabled
 
-    private var _button1NextEvent = MutableValue(0)
+    private var _button1NextEvent = MutableValue(gameButton1NextEvent)
     var button1NextEvent:Value<Int> = _button1NextEvent
-    private var _button2NextEvent = MutableValue(0)
+    private var _button2NextEvent = MutableValue(gameButton2NextEvent)
     var button2NextEvent:Value<Int> = _button2NextEvent
-    private var _button3NextEvent = MutableValue(0)
+    private var _button3NextEvent = MutableValue(gameButton3NextEvent)
     var button3NextEvent:Value<Int> = _button3NextEvent
-    private var _button4NextEvent = MutableValue(0)
+    private var _button4NextEvent = MutableValue(gameButton4NextEvent)
     var button4NextEvent:Value<Int> = _button4NextEvent
-    private var _button5NextEvent = MutableValue(0)
+    private var _button5NextEvent = MutableValue(gameButton5NextEvent)
     var button5NextEvent:Value<Int> = _button5NextEvent
-    private var _button6NextEvent = MutableValue(0)
+    private var _button6NextEvent = MutableValue(gameButton6NextEvent)
     var button6NextEvent:Value<Int> = _button6NextEvent
 
-    private var _companyFinances = MutableValue(10000000000)
+    private var _companyFinances = MutableValue(gameCompanyFinances)
     var companyFinances: Value<Long> = _companyFinances
-    var _gameStatus = MutableValue("start")
+    var _gameStatus = MutableValue(gameGameStatus)
     var gameStatus: Value<String> = _gameStatus
-    var _gameCrewStatus = MutableValue(0)
+    var _gameCrewStatus = MutableValue(gameShipCrewStatus)
     var gameCrewStatus: Value<Int> = _gameCrewStatus
-    var _gameShipHull = MutableValue(0)
+    var _gameShipHull = MutableValue(gameShipHullStatus)
     var gameShipHull: Value<Int> = _gameShipHull
-    var _gameShipSensors = MutableValue(0)
+    var _gameShipSensors = MutableValue(gameShipSensorStatus)
     var gameShipSensors: Value<Int> = _gameShipSensors
-    var _gameShipEngines = MutableValue(0)
+    var _gameShipEngines = MutableValue(gameShipEnginesStatus)
     var gameShipEngines: Value<Int> = _gameShipEngines
-    var _gameShipDestination = MutableValue("UNKN")
+    var _gameShipDestination = MutableValue(gameDestinationStatus)
     var gameShipDestination: Value<String> = _gameShipDestination
-    var _gameTime = MutableValue(50.0)
+    var _gameTime = MutableValue(gameGameTime)
     var gameTime: Value<Double> = _gameTime
 
-    var _ceoFirstname = MutableValue(gameCEOFirstname)
+    var _ceoFirstname = MutableValue(gameCeoFirstname)
     var ceoFirstname:Value<String> = _ceoFirstname
 
-    var _ceoLastname = MutableValue(gameCEOLastname)
+    var _ceoLastname = MutableValue(gameCeoLastName)
     var ceoLastname:Value<String> = _ceoLastname
 
     var _companyName = MutableValue(gameCompanyName)
